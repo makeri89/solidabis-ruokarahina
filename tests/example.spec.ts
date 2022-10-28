1d98413de7bbf430f6a6ab5eea8113440c4e7c31
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-10-28 15:00:49 +0300

    Added baseurl to playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('front page tests', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000')
+    await page.goto('/')
   })
 
   test('front page has correct content', async ({ page }) => {
