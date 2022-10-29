374bda2ab1e5d6da2f9fd23df7761ba61f06fd13
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-10-29 12:52:38 +0300

    Added revalidation

--- src/lib/hooks/useIngredient.ts
+++ src/lib/hooks/useIngredient.ts
@@ -2,7 +2,9 @@ import { fetcher } from '@lib/utils'
 import useSWR from 'swr'
 
 export const useIngredient = (id?: string) => {
-  const { data, error } = useSWR(id ? `/api/ingredient/${id}` : null, fetcher)
+  const { data, error } = useSWR(id ? `/api/ingredient/${id}` : null, fetcher, {
+    refreshInterval: 2000,
+  })
 
   return {
     ingredient: data,
